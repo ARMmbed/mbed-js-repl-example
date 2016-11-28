17460eb57893bd91be4fe7c7f7f4593db02a1f5c
Jan Jongboom <janjongboom@gmail.com>
2016-11-29 00:30:10 +0800

    Add message on how to stop LED from blinking

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
-var repl = new JSRepl();
-
 var led = new DigitalOut(LED1);
-setInterval(function() {
+var iv = setInterval(function() {
   led.write(led.read() ? 0 : 1);
 }, 500);
+
+print("To stop LED1 from blinking, call `clearInterval(" + iv + ")`");
+
+var repl = new JSRepl();
