8ed5209333c28a45f060e305da99c6d36891166d
Jan Jongboom <janjongboom@gmail.com>
2016-11-28 19:36:14 +0800

    Remove reference to iv

--- main.js
+++ main.js
@@ -1,8 +1,6 @@
 var repl = new JSRepl();
 
 var led = new DigitalOut(LED1);
-var iv = setInterval(function() {
+setInterval(function() {
   led.write(led.read() ? 0 : 1);
 }, 500);
-
-// in the REPL run: `clearInterval(iv)` to stop LED1 from blinking
